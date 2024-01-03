4b905ce707be92db3e50bfd5a3172752d1755def
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-01-03 13:09:28 +0200

    Data Types and Table Basics - Lab

---
+++ postgresql tasks/1-2.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+    employees(first_name, last_name, hiring_date, salary, devices_number)
+VALUES
+       ('John', 'Doe', '2022-02-02', 3000, 1),
+       ('Jo', 'Biden', '2020--06-04', 10000, 2),
+       ('Dean', 'Fraser', '2021--05-30', 4000, 3);
