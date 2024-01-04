3c3b7a8ac9ed6f5de642c22983b8f36e45d3a01b
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-01-04 13:17:58 +0200

    Basic CRUD - Exercise

---
+++ postgresql tasks/2-25.sql
@@ -0,0 +1,9 @@
+CREATE TABLE company_chart
+AS
+SELECT
+    CONCAT_WS(' ', first_name, last_name) AS "Full Name",
+    job_title AS "Job Title",
+    department_id AS "Department ID",
+    manager_id AS "Manager ID"
+FROM
+    employees;
