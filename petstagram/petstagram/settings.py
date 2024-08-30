4b2e37a22a8e89eab7d766228eb69881a16bc81c
Zlatin Rusev <zlatinrusev@gmail.com>
2024-08-30 11:01:01 +0200

    Changes Commited

--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'petstagram.photos.apps',
+    'petstagram.pets.apps',
+    'petstagram.accounts.apps',
+    'petstagram.common.apps',
 ]
 
 MIDDLEWARE = [
