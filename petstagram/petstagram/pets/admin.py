fa4b1d8fe21c8712a0f864a9eb0557353eb828fa
Zlatin Rusev <zlatinrusev@gmail.com>
2024-08-30 10:52:06 +0200

    Initial Commit

--- petstagram/petstagram/pets/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from petstagram.pets.models import Pet
-
-# admin.site.register(Pet)  <- Other way to register models in the admin panel (not recommended)
-
-@admin.register(Pet)
-class PetAdmin(admin.ModelAdmin):
-    list_display = ('name', 'date_of_birth', 'slug')
