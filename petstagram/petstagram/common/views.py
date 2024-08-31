bc90c5d15ba680bba482975458fdce619fdad821
Zlatin Rusev <zlatinrusev@gmail.com>
2024-08-31 17:41:58 +0200

    Changes Commited

--- petstagram/petstagram/common/views.py
+++ petstagram/petstagram/common/views.py
@@ -3,9 +3,6 @@ from django.shortcuts import render
 from petstagram.photos.models import Photo
 
 
-# Create your views here.
-
-
 def index(request):
     context = {
         "pet_photos": Photo.objects.all()
