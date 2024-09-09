00fb1880a9c1e40956dcef7cc40999e9317c2d51
Zlatin Rusev <zlatinrusev@gmail.com>
2024-09-09 19:16:49 +0200

    Changes Commited

--- petstagram/petstagram/common/views.py
+++ petstagram/petstagram/common/views.py
@@ -22,6 +22,7 @@ def like_pet_photo(request, pk):
     # pet_photo = Photo.objects.get(pk=pk, user=request.user)
     # pet_photo = Photo.objects.get(pk=pk)
     # pet_photo_like = PhotoLike.objects.first(pk=pk, user=request.user)
+
     pet_photo_like = PhotoLike.objects.filter(pet_photo_id=pk).first()
 
     if pet_photo_like:
