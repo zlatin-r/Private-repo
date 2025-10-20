b900fa459e8bfddb5fd74a1197f286f97f3549f4
zlatin-r <zlatinrusev@gmail.com>
2025-10-20 20:54:45 +0300

    solutions added

--- softuni/Python Basics/While Loop - Exercise/03.vacantion.py
+++ softuni/Python Basics/While Loop - Exercise/03.vacantion.py
@@ -1,29 +1,27 @@
-money_needed = float(input())
-money_have = float(input())
-days_spend = 0
-total_days = 0
+needed_money = float(input())
+owned_money = float(input())
 
-while True:
-    acton = input()
-    amount = float(input())
+days_counter = 0
+spending_counter = 0
 
-    total_days += 1
+while owned_money < needed_money and spending_counter < 5:
+    command = input()
+    money = float(input())
+    days_counter += 1
 
-    if acton == "spend":
-        days_spend += 1
+    if command == "save":
+        owned_money += money
+        spending_counter = 0
 
-        if days_spend == 5:
-            print(f"You can't save the money.\n{days_spend}")
-            break
+    elif command == "spend":
+        owned_money -= money
+        spending_counter += 1
+        if owned_money < 0:
+            owned_money = 0
 
-        money_have -= amount
+    if spending_counter == 5:
+        print("You can't save the money.")
+        print(days_counter)
 
-        if money_have < 0:
-            money_have = 0
-        
-    if acton == "save":
-        money_have += amount
-
-        if money_have >= money_needed:
-            print(f"You saved the money for {total_days} days.")
-            break
+    if owned_money >= needed_money:
+        print(f"You saved the money for {days_counter} days.")
