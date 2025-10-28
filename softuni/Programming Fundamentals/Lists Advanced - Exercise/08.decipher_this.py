4638e7cd2c4fcd9dbdf6fc1fe10b2138d1ecaf8c
zlatin-r <zlatinrusev@gmail.com>
2025-10-28 16:29:51 +0200

    solutions added

--- softuni/Programming Fundamentals/Lists Advanced - Exercise/08.decipher_this.py
+++ softuni/Programming Fundamentals/Lists Advanced - Exercise/08.decipher_this.py
@@ -1,7 +1,17 @@
 message = input().split()
 
-decoded = [chr(int("".join(filter(str.isdigit, x)))) for x in message]
+decoded_words = []
 
-decoded_message = "".join(decoded)
+for word in message:
+    digits = "".join(filter(str.isdigit, word))
+    first_char = chr(int(digits))
+    
+    rest = word[len(digits):]
+    
+    if len(rest) > 1:
+        rest = rest[-1] + rest[1:-1] + rest[0]
+    
+    decoded_words.append(first_char + rest)
 
+decoded_message = " ".join(decoded_words)
 print(decoded_message)
