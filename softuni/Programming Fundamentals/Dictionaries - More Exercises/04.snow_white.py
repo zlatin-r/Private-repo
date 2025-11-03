3ac77d7293e475158c7806875e11c8044e52b5ff
zlatin-r <zlatinrusev@gmail.com>
2025-11-03 16:29:59 +0200

    solutions added

--- softuni/Programming Fundamentals/Dictionaries - More Exercises/04.snow_white.py
+++ softuni/Programming Fundamentals/Dictionaries - More Exercises/04.snow_white.py
@@ -6,19 +6,25 @@ while command != "Once upon a time":
     name, color, physics = command.split(" <:> ")
     physics = int(physics)
 
-    if color in data and name in data[color]:
-        data[color][name] = max(data[color][name], physics)
-
     if color not in data:
         data[color] = {}
 
-    if name not in data[color]:
+    if name in data[color]:
+        if data[color][name] < physics:
+            data[color][name] = physics
+    else:
         data[color][name] = physics
-    
+
     command = input()
 
-sorted_data = sorted(data.items(), key=lambda x: -sum(x[1].values()))
+flat_dwarfs = []
+for color, dwarfs in data.items():
+    for name, physics in dwarfs.items():
+        flat_dwarfs.append((name, color, physics))
 
-for col, info in sorted_data:
-    for name, points in info.items():
-        print(f"({col}) {name} <-> {points}")
+color_counts = {color: sum(len(d) for c, d in data.items() if c == color) for color in data}
+
+flat_dwarfs.sort(key=lambda x: (-x[2], -color_counts[x[1]]))
+
+for name, color, physics in flat_dwarfs:
+    print(f"({color}) {name} <-> {physics}")
