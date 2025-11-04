a406de8736781cfa6dd316b7bd9783f4abbe06e9
zlatin-r <zlatinrusev@gmail.com>
2025-11-04 17:34:26 +0200

    solution added

--- softuni/Python OOP/Testing - Lab/02.test_cat.py
+++ softuni/Python OOP/Testing - Lab/02.test_cat.py
@@ -5,7 +5,7 @@ class TestCat(TestCase):
     def setUp(self):
         self.cat = Cat("Tom")
 
-    def test_if_cat_size_increased_after_eating(self):
+    def test_if_size_increased_after_eating(self):
         expected_size = self.cat.size + 1
 
         self.cat.eat()
@@ -13,6 +13,13 @@ class TestCat(TestCase):
         self.assertTrue(self.cat.fed)
         self.assertTrue(self.cat.sleepy)
         self.assertEqual(expected_size, self.cat.size)
+
+    def test_if_fed_after_eating(self):
+        self.cat.fed = False
+
+        self.cat.eat()
+
+        self.assertTrue(self.cat.fed)
 
     def test_cant_eat_after_fed_is_true(self):
         self.cat.fed = True
@@ -22,7 +29,22 @@ class TestCat(TestCase):
 
         self.assertEqual("Already fed.", str(ex.exception))
 
-    
+    def test_cant_fall_sleep_if_not_fed(self):
+        self.cat.fed = False
+
+        with self.assertRaises(Exception) as ex:
+            self.cat.sleep()
+
+        self.assertEqual("Cannot sleep while hungry", str(ex.exception))
+
+    def test_if_sleepy_after_sleeping(self):
+        self.cat.fed = True
+        self.cat.sleepy = True
+
+        self.cat.sleep()
+
+        self.assertFalse(self.cat.sleepy)
+
 
 if __name__ == "__main__":
     main()
