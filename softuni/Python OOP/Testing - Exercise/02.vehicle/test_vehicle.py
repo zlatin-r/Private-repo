66fbd3aba0f98bb9b406dd965cd39b78ebaafe2c
zlatin-r <zlatinrusev@gmail.com>
2025-11-09 17:10:01 +0200

    solution added

--- softuni/Python OOP/Testing - Exercise/02.vehicle/test_vehicle.py
+++ softuni/Python OOP/Testing - Exercise/02.vehicle/test_vehicle.py
@@ -5,6 +5,12 @@ class TestVehicle(TestCase):
     def setUp(self):
         self.vehicle = Vehicle(100.0, 200.0)
 
+    def test_correct_init(self):
+        self.assertEqual(100, self.vehicle.fuel)
+        self.assertEqual(self.vehicle.capacity, self.vehicle.fuel)
+        self.assertEqual(self.vehicle.horse_power, self.vehicle.horse_power)
+        self.assertEqual(self.vehicle.DEFAULT_FUEL_CONSUMPTION, self.vehicle.fuel_consumption)
+
 
 if __name__ == "__main__":
     main()
