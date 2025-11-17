4332102371e996b61c57e17cd0f3f578157029e6
zlatin-r <zlatinrusev@gmail.com>
2025-11-17 07:38:54 +0200

    solution added

---
+++ JS Front End/JS Syntax Fundamentals - Lab/05.math_operations.js
@@ -0,0 +1,23 @@
+function solve(num1, num2, string) {
+    switch (string) {
+        case "+":
+            console.log(num1 + num2);
+            break;
+        case "-":
+            console.log(num1 - num2);
+            break;
+        case "*":
+            console.log(num1 * num2);
+            break;
+        case "/":
+            console.log(num1 / num2);
+            break;
+        case "%":
+            console.log(num1 % num2);
+            break;
+        case "**":
+            console.log(num1 ** num2);
+            break;        
+    }
+}
+
