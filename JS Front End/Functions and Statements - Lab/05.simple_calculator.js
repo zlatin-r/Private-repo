2100de19a9b2630d6ad3e21aa37e4bffd433ebe3
zlatin-r <zlatinrusev@gmail.com>
2025-11-22 18:59:58 +0200

    solution added

---
+++ JS Front End/Functions and Statements - Lab/05.simple_calculator.js
@@ -0,0 +1,12 @@
+function solve(a, b, op) {
+    switch (op) {
+        case "multiply":
+            console.log(a * b); break;
+        case "divide":
+            console.log(a / b); break;
+        case "add":
+            console.log(a + b); break;
+        case "subtract":
+            console.log(a - b); break;
+    }
+}
