4c8e07577cb9f0b54a3a2a6a9fdbad502d32200b
zlatin-r <zlatinrusev@gmail.com>
2025-11-22 18:03:11 +0200

    solution added

---
+++ JS Front End/Functions and Statements - Lab/01.format_grade.js
@@ -0,0 +1,18 @@
+function solve(grade) {
+    if (grade < 3) {
+        console.log(`Fail (2)`);
+    } else if (grade >= 3 && grade < 3.50) {
+        console.log(`Poor (${grade.toFixed(2)})`);
+    } else if (grade >= 3.50 && grade < 4.50) {
+        console.log(`Good (${grade.toFixed(2)})`);
+    } else if (grade >= 4.50 && grade < 5.50) {
+        console.log(`Very good (${grade.toFixed(2)})`);
+    } else if (grade >= 5.50) {
+        console.log(`Excellent (${grade.toFixed(2)})`);
+    }
+}
+
+solve(3)
+solve(3.70)
+solve(4.70)
+solve(5.70)
