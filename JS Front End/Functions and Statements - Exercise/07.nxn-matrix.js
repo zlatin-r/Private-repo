5317edf704ccb962ee434ef91dda6c3f95f90dbd
zlatin-r <zlatinrusev@gmail.com>
2025-11-24 06:26:41 +0200

    solution added

---
+++ JS Front End/Functions and Statements - Exercise/07.nxn-matrix.js
@@ -0,0 +1,11 @@
+function nXnMatrix(n) {
+    for (let i = 0; i < n; i++) {
+        let row = "";
+        for (let j = 0; j < n; j++) {
+            row += n + " "
+        }
+        console.log(row.trim());
+    }
+}
+
+nXnMatrix(5);
