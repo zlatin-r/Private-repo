8ec541d2ffaa01c1b0dd8f761ca2bd4bef8cd833
zlatin-r <zlatinrusev@gmail.com>
2025-12-01 06:33:11 +0200

    solution added

--- JS Front End/DOM Introduction - Lab/02.Collect List Items/collect-list-items.js
+++ JS Front End/DOM Introduction - Lab/02.Collect List Items/collect-list-items.js
@@ -1,3 +1,6 @@
 function extractText() {
-    // TODO
+    const listItems = document.getElementsByTagName('li');
+    const textArea = document.querySelector('#result');
+
+    textArea.value = Array.from(listItems).map(item => item.textContent).join('\n');
 }
