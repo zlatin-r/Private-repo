41ca5fe6db3d126642b2ab4e4042cdfbd76e94f4
zlatin-r <zlatinrusev@gmail.com>
2025-12-01 07:05:51 +0200

    solution added

--- JS Front End/DOM Introduction - Lab/06.Sum Table/sum-table.js
+++ JS Front End/DOM Introduction - Lab/06.Sum Table/sum-table.js
@@ -1,3 +1,8 @@
 function sumTable() {
-    // TODO
+    const tableCells = document.querySelectorAll('table tbody tr:not(:last-child) td:last-child');
+    const sumElement = document.querySelector('#sum');
+
+    sumElement.textContent = [...tableCells]
+        .map(el => Number(el.textContent))
+        .reduce((a, b) => a + b, 0);
 }
