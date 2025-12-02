014dbb94b47fd91f8ff3aeca6667d6cad3d6d0b5
zlatin-r <zlatinrusev@gmail.com>
2025-12-02 06:50:23 +0200

    solution added

--- JS Front End/DOM Introduction - Exercises/03. Accordion/accordion.js
+++ JS Front End/DOM Introduction - Exercises/03. Accordion/accordion.js
@@ -1,3 +1,11 @@
 function toggle() {
-    //TODO
+    let buttonTextElement = document.getElementsByClassName('button')[0].textContent;
+
+    if (buttonTextElement === "More") {
+        document.querySelector("#extra").style.display = "block";
+        document.getElementsByClassName('button')[0].textContent = "Less";
+    } else {
+        document.querySelector("#extra").style.display = "none";
+        document.getElementsByClassName('button')[0].textContent = "More";
+    }
 }
