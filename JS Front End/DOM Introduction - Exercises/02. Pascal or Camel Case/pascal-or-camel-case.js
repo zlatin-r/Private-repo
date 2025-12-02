d4405547e42af6071f9040a1e2fba1dac4576a70
zlatin-r <zlatinrusev@gmail.com>
2025-12-02 06:42:42 +0200

    solution added

--- JS Front End/DOM Introduction - Exercises/02. Pascal or Camel Case/pascal-or-camel-case.js
+++ JS Front End/DOM Introduction - Exercises/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO
+    let text = document.getElementById('text').value;
+    let convention = document.getElementById('naming-convention').value;
+    let resultField = document.getElementById('result');
+
+    let words = text.toLowerCase().split(" ");
+    let result = "";
+
+    if (convention === "Camel Case") {
+        result = words
+            .map((w, i) =>
+                i === 0 ? w : w.charAt(0).toUpperCase() + w.slice(1)
+            )
+            .join("");
+    } else if (convention === "Pascal Case") {
+        result = words
+            .map(w => w.charAt(0).toUpperCase() + w.slice(1))
+            .join("");
+    } else {
+        result = "Error!";
+    }
+
+    resultField.textContent = result;
 }
