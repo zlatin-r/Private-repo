3f48bbae8796f6bc1a174e6f31fc68a9bfe4b4b8
zlatin-r <zlatinrusev@gmail.com>
2025-12-03 06:34:25 +0200

    solution added

--- JS Front End/DOM Introduction - Exercises/04. Search in List/search-in-list.js
+++ JS Front End/DOM Introduction - Exercises/04. Search in List/search-in-list.js
@@ -1,3 +1,18 @@
 function solve() {
-   // TODO
+    const townNameElements = document.querySelectorAll("#towns li");
+    const searchedElementStr = document.querySelector("#searchText").value;
+    const result = document.querySelector("#result");
+
+    let matchesFound = 0;
+
+    townNameElements.forEach(town => {
+        const townName = town.textContent.trim();
+
+        if (townName.includes(searchedElementStr)) {
+            matchesFound++;
+            town.style.fontWeight = "bold";
+            town.style.textDecoration = "underline";
+        }
+    });
+    result.textContent = `${matchesFound} matches found`;
 }
