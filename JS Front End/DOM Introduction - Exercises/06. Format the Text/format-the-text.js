71929aecec5be11303387c54818ecf2fcc89bedf
zlatin-r <zlatinrusev@gmail.com>
2025-12-03 06:51:56 +0200

    solution added

--- JS Front End/DOM Introduction - Exercises/06. Format the Text/format-the-text.js
+++ JS Front End/DOM Introduction - Exercises/06. Format the Text/format-the-text.js
@@ -1,3 +1,16 @@
 function solve() {
-  // TODO
+    const textElement = document.querySelector('#input').value;
+    const outputElement = document.querySelector('#output');
+    const sentences = textElement
+        .split(".")
+        .filter(s => s.length > 0)
+        .map(s => s + ".");
+
+    for (let i = 0; i < sentences.length; i += 3) {
+        const group = sentences.slice(i, i + 3);
+        const newParagraphElement = document.createElement('p');
+
+        newParagraphElement.textContent = group.join('').trim();
+        outputElement.appendChild(newParagraphElement);
+    }
 }
