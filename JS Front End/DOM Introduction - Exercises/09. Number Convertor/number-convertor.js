706ad6f802be0d2b908f14850c50b8ee9ea6087c
zlatin-r <zlatinrusev@gmail.com>
2025-12-05 06:30:57 +0200

    solution added

--- JS Front End/DOM Introduction - Exercises/09. Number Convertor/number-convertor.js
+++ JS Front End/DOM Introduction - Exercises/09. Number Convertor/number-convertor.js
@@ -1,3 +1,14 @@
 function solve() {
-    // TODO
+    const input = Number(document.querySelector('#input').value);
+    const convertTo = document.querySelector('#selectMenuTo').value.toLowerCase();
+    const resultEl = document.querySelector('#result');
+
+    switch (convertTo) {
+        case 'binary':
+            resultEl.value = input.toString(2);
+            break;
+        case 'hexadecimal':
+            resultEl.value = input.toString(16).toUpperCase();
+            break;
+    }
 }
