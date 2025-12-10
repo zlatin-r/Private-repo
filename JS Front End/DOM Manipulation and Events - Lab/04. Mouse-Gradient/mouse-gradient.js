f1250bb373ea217d0c1956b24bd562c7ba84bff5
zlatin-r <zlatinrusev@gmail.com>
2025-12-10 07:00:26 +0200

    solution added

--- JS Front End/DOM Manipulation and Events - Lab/04. Mouse-Gradient/mouse-gradient.js
+++ JS Front End/DOM Manipulation and Events - Lab/04. Mouse-Gradient/mouse-gradient.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    //TODO
+    const resultEl = document.querySelector('#result');
+    const gradientEl = document.querySelector('#gradient');
+
+    gradientEl.addEventListener('mousemove', (e) => {
+        const currentPosition = e.offsetX;
+        const elementWidth = e.currentTarget.clientWidth;
+
+        const percentage = Math.floor((currentPosition / elementWidth) * 100);
+
+        resultEl.textContent = `${percentage}%`;
+    });
 }
