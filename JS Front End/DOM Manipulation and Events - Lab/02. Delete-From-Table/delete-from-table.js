952d6f38c22ab94f8aa3bbf2eb15e0a3ac7d9ced
zlatin-r <zlatinrusev@gmail.com>
2025-12-10 06:25:59 +0200

    solution added

--- JS Front End/DOM Manipulation and Events - Lab/02. Delete-From-Table/delete-from-table.js
+++ JS Front End/DOM Manipulation and Events - Lab/02. Delete-From-Table/delete-from-table.js
@@ -3,7 +3,7 @@ function deleteByEmail() {
     let emailList = document.querySelectorAll('#customers tr td:nth-child(2)');
 
     for (let td of emailList) {
-        if (td.textContent == email) {
+        if (td.textContent === email) {
             let row = td.parentNode;
             row.parentNode.removeChild(row);
             document.getElementById('result').textContent = 'Deleted.';
