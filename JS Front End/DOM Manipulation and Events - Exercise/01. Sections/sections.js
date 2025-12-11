06c34e0b30f1236f4cf484028e8e1b889606fbf7
zlatin-r <zlatinrusev@gmail.com>
2025-12-11 07:10:01 +0200

    solution added

--- JS Front End/DOM Manipulation and Events - Exercise/01. Sections/sections.js
+++ JS Front End/DOM Manipulation and Events - Exercise/01. Sections/sections.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-   // TODO
+   let inputText = document.querySelector('#task-input input[type="text"]').value;
+   let resultContent = document.querySelector('#content');
+   let sections = inputText.split(', ');
+
+   sections.forEach(section => {
+       let newDivElement = document.createElement('div');
+       let newParagraph = document.createElement('p');
+
+       newDivElement.appendChild(newParagraph);
+       resultContent.appendChild(newDivElement);
+   });
 }
