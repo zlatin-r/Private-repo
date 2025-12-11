a3414ea2c1ce5e5f4c0473d6f22f060391afb60e
zlatin-r <zlatinrusev@gmail.com>
2025-12-11 06:30:53 +0200

    solution added

--- JS Front End/DOM Manipulation and Events - Lab/05. Highlight-Active/hightlight-active.js
+++ JS Front End/DOM Manipulation and Events - Lab/05. Highlight-Active/hightlight-active.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    function sectionFocusedEventHandler(event) {
+        event.target.parentElement.classList.add('focused');
+    }
+
+    function sectionBlurEventHandler(event) {
+        event.target.parentElement.classList.remove('focused');
+    }
+
+    const inputFields = document.querySelectorAll('input[type="text"]');
+
+    inputFields.forEach(el => {
+        el.addEventListener('focus', sectionFocusedEventHandler);
+        el.addEventListener('blur', sectionBlurEventHandler);
+    });
 }
