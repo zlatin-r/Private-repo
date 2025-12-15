63a92946a12012f150127c5f68ed1b1f4b6d6f2a
zlatin-r <zlatinrusev@gmail.com>
2025-12-15 06:41:12 +0200

    solution added

--- JS Front End/DOM Manipulation and Events - Exercise/02. Time-Converter/time-converter.js
+++ JS Front End/DOM Manipulation and Events - Exercise/02. Time-Converter/time-converter.js
@@ -1,5 +1,43 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    let inputValue = document.querySelector('input');
+    const allButtons = document.querySelectorAll('input[type="submit"]');
+
+    const daysEl = document.querySelector('#days-input');
+    const hoursEl = document.querySelector('#hours-input');
+    const minutesEl = document.querySelector('#minutes-input');
+    const secondsEl = document.querySelector('#seconds-input');
+
+    allButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            const parentElement = event.target.parentElement;
+            const timeType = event.target.parentElement.id;
+            const timeValueEl = parentElement.querySelector('input[type="number"]');
+            const timeValue = parseFloat(timeValueEl.value).toFixed(2);
+
+            switch (timeType) {
+                case 'days':
+                    hoursEl.value = `${(timeValue * 24).toFixed(2)}`;
+                    minutesEl.value = `${(timeValue * 1440).toFixed(2)}`;
+                    secondsEl.value = `${(timeValue * 86400).toFixed(2)}`;
+                    break;
+                case 'hours':
+                    daysEl.value = `${(timeValue / 24).toFixed(2)}`;
+                    minutesEl.value = `${(timeValue * 60).toFixed(2)}`;
+                    secondsEl.value = `${(timeValue * 3600).toFixed(2)}`;
+                    break;
+                case 'minutes':
+                    daysEl.value = `${(timeValue / 1440).toFixed(2)}`;
+                    hoursEl.value = `${(timeValue / 60).toFixed(2)}`;
+                    secondsEl.value = `${(timeValue * 60).toFixed(2)}`;
+                    break;
+                case 'seconds':
+                    daysEl.value = `${(timeValue / 86400).toFixed(2)}`;
+                    hoursEl.value = `${(timeValue / 3600).toFixed(2)}`;
+                    minutesEl.value = `${(timeValue / 60).toFixed(2)}`;
+            }
+        });
+    });
 }
