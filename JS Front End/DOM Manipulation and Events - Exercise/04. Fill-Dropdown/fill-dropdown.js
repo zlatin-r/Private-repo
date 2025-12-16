b9fb5f865a7226d3a6994ba6ad3284857c4e95e8
zlatin-r <zlatinrusev@gmail.com>
2025-12-16 07:17:08 +0200

    solution added

--- JS Front End/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/fill-dropdown.js
+++ JS Front End/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/fill-dropdown.js
@@ -1,5 +1,21 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const text = document.getElementById('newItemText');
+    const value = document.getElementById('newItemValue');
+    const form = document.querySelector('form');
+
+    const menuElement = document.getElementById('menu');
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const newOptionEl = document.createElement('option');
+
+        [newOptionEl.textContent, newOptionEl.value] = [text.value, value.value];
+
+        menuElement.appendChild(newOptionEl);
+
+        [text.value, value.value] = ["", ""];
+    });
 }
