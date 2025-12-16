3587deb821601936763fb04fb8fbad900370a461
zlatin-r <zlatinrusev@gmail.com>
2025-12-16 06:53:17 +0200

    solution added

--- JS Front End/DOM Manipulation and Events - Exercise/01. Sections/sections.js
+++ JS Front End/DOM Manipulation and Events - Exercise/01. Sections/sections.js
@@ -1,29 +1,30 @@
-document.addEventListener('DOMContentLoaded', solve);
-
 function solve() {
 
     const contendElement = document.querySelector('#content');
     const generateElement = document.querySelector('input[type="submit"]');
 
     generateElement.addEventListener('click', (e) => {
+        // generateElement.disabled = true;
         e.preventDefault();
 
-        const inputStrings = document.querySelector('input[type="text"]').value.split(', ');
+        const inputStrings = document.querySelector('input[type="text"]').value;
 
-        inputStrings.forEach((str) => {
+        if (inputStrings.trim() !== "") {
+            const arrStrings = inputStrings.split(', ');
+            arrStrings.forEach((str) => {
 
-            const newDiv = document.createElement('div');
-            const newParagraph = document.createElement('p');
+                const newDiv = document.createElement('div');
+                const newParagraph = document.createElement('p');
 
-            newParagraph.textContent = str;
-            newParagraph.style.display = 'none';
+                newParagraph.textContent = str;
+                newParagraph.style.display = 'none';
 
-            newDiv.appendChild(newParagraph);
-            newDiv.addEventListener('click', (e) => {
-                e.target.querySelector('p').style.display = 'block';
+                newDiv.appendChild(newParagraph);
+                newDiv.addEventListener('click', (e) => {
+                    e.target.querySelector('p').style.display = 'block';
+                });
+                contendElement.appendChild(newDiv);
             });
-
-            contendElement.appendChild(newDiv);
-        });
+        }
     });
 }
