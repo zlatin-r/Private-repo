a1b395a9d4b910fc0d350cef23a9f7fa08e25d70
zlatin-r <zlatinrusev@gmail.com>
2025-12-17 06:47:48 +0200

    solution added

--- JS Front End/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
+++ JS Front End/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
@@ -1,5 +1,44 @@
 document.addEventListener('DOMContentLoaded', solve);
 
+function encode(text) {
+    let result = '';
+
+    for (let i = 0; i < text.length; i++) {
+        const charCode = text.charCodeAt(i);
+        result += String.fromCharCode(charCode + 1);
+    }
+
+    return result;
+}
+
+function decode(text) {
+    let result = '';
+
+    for (let i = 0; i < text.length; i++) {
+        const charCode = text.charCodeAt(i);
+        result += String.fromCharCode(charCode - 1);
+    }
+
+    return result;
+}
+
 function solve() {
-    //TODO
+    let message = document.querySelector('#encode textarea');
+    let receivedMessage = document.querySelector('#decode textarea');
+
+    const encodeBtn = document.querySelector('#encode button');
+    const decodeBtn = document.querySelector('#decode button');
+
+    encodeBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        receivedMessage.value = encode(message.value);
+        message.value = "";
+    })
+
+    decodeBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        receivedMessage.value = decode(receivedMessage.value);
+    })
 }
