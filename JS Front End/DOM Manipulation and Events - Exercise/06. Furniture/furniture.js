de6206f7a130892d469fa0e54a8b063c01dc2e32
zlatin-r <zlatinrusev@gmail.com>
2025-12-18 06:35:34 +0200

    solution added

--- JS Front End/DOM Manipulation and Events - Exercise/06. Furniture/furniture.js
+++ JS Front End/DOM Manipulation and Events - Exercise/06. Furniture/furniture.js
@@ -5,8 +5,24 @@ function solve() {
     const data = JSON.parse(itemsData.value)
 
     for (const item of data) {
-        console.log(item.name);
-        console.log(item.price);
-        console.log(item.decFactor);
+        let itemImage = document.createElement('img');
+        itemImage.textContent = item.img;
+
+        let itemName = document.createElement('p');
+        itemName.textContent = item.name;
+
+        let itemPrice = document.createElement('p');
+        itemPrice.textContent = item.price;
+
+        let itemDecFactor = document.createElement('p');
+        itemDecFactor.textContent = item.itemDecFactor;
+
+        let checkBox = document.createElement('input');
+        checkBox.type = 'checkbox';
+
+        // console.log(item.img);
+        // console.log(item.name);
+        // console.log(item.price);
+        // console.log(item.decFactor);
     }
 }
