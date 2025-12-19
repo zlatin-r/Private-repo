76347dc7f121a109095e54f51e00865a5d55b52e
zlatin-r <zlatinrusev@gmail.com>
2025-12-19 07:00:05 +0200

    solution added

--- JS Front End/DOM Manipulation and Events - Exercise/07. Distance-Converter/distance-converter.js
+++ JS Front End/DOM Manipulation and Events - Exercise/07. Distance-Converter/distance-converter.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const inputUnitSelect = document.getElementById('inputUnits');
+    const outputUnitSelect = document.getElementById('outputUnits');
+    const inputUnit = inputUnitSelect.value;
+    const outputUnit = outputUnitSelect.value;
+    const inputValue = document.getElementById('inputDistance').value;
+    let outputValue = document.getElementById('outputDistance').value;
+
+
+
+
 }
