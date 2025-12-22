576cf44fd3199b1ba5e8ade9414434a405af1dfe
zlatin-r <zlatinrusev@gmail.com>
2025-12-22 06:44:30 +0200

    solution added

--- JS Front End/DOM Manipulation and Events - Exercise/08. Sudomu/sudomu.js
+++ JS Front End/DOM Manipulation and Events - Exercise/08. Sudomu/sudomu.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const checkBtn = document.querySelector('.buttons input[type="submit"]');
+    const resetBtn = document.querySelector('.buttons input[type="reset"]');
+
+    const firstRowFirstColCell = document.querySelector('')
 }
