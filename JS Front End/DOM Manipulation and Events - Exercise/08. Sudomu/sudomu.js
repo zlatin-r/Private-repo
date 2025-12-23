840602885b2fda593bd4b54bd39cecc643b7b581
zlatin-r <zlatinrusev@gmail.com>
2025-12-23 06:37:03 +0200

    solution added

--- JS Front End/DOM Manipulation and Events - Exercise/08. Sudomu/sudomu.js
+++ JS Front End/DOM Manipulation and Events - Exercise/08. Sudomu/sudomu.js
@@ -1,35 +1,41 @@
-document.addEventListener('DOMContentLoaded', solve);
-
 function solve() {
-    const inputs = document.querySelectorAll('#solutionCheck td input');
+    const form = document.getElementById('solutionCheck');
+    const table = document.querySelector('table');
+    const checkResult = document.getElementById('check');
 
-    document.querySelector('.buttons').addEventListener('click', (e) => {
+    form.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        const values = Array.from(inputs).map(input => input.value);
+        const inputs = Array.from(
+            table.querySelectorAll('input[type="number"]')
+        );
 
-        if (e.target.value === 'Quick Check') {
-            const rowsAndCells = [];
+        const values = inputs.map(input => Number(input.value));
 
-            rowsAndCells.push(
-                [values[0], values[1], values[2]],
-                [values[3], values[4], values[5]],
-                [values[6], values[7], values[8]],
-                [values[0], values[3], values[6]],
-                [values[1], values[4], values[7]],
-                [values[2], values[5], values[8]],
-            );
-            const allValid = rowsAndCells.every(
-                arr => new Set(arr).size === arr.length
-            );
+        const rowsAndCols = [
+            [values[0], values[1], values[2]],
+            [values[3], values[4], values[5]],
+            [values[6], values[7], values[8]],
+            [values[0], values[3], values[6]],
+            [values[1], values[4], values[7]],
+            [values[2], values[5], values[8]],
+        ];
 
-            if (allValid) {
-                document.querySelector('#check').textContent = "Success!";
-            } else {
-                document.querySelector('#check').textContent = "Keep trying...";
-            }
-        } else if (e.target.value === 'Clear') {
+        const allValid = rowsAndCols.every(
+            arr => new Set(arr).size === arr.length
+        );
 
-        }
-    })
+        table.style.border = allValid
+            ? '2px solid green'
+            : '2px solid red';
+
+        checkResult.textContent = allValid
+            ? 'Success!'
+            : 'Keep trying ...';
+    });
+
+    form.addEventListener('reset', () => {
+        checkResult.textContent = '';
+        table.style.border = '';
+    });
 }
